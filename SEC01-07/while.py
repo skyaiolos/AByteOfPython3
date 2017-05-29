f76d198d3fdbd254d391875a9a44ab21b21b3026
skyaiolos <skyaiolos@aliyun.com>
2017-05-29 13:20:23 +0800

    update for supporting the python 3.6.2

--- SEC01-07/while.py
+++ SEC01-07/while.py
@@ -1,18 +1,22 @@
 # !/usr/bin/python
 # Filename while.py
 
-number=23
-running=True
-
-while running:
-  guess=int(input("Enter an integer:"))
-  if guess==number:
-        print("Congratulation, you guessd it.")
-        running=False #this causes the while loop to stop
-  elif guess<number:
-        print("No, it is a little higher")
-  else:
-        print("No, it is a little lower")
+number = 23
+# running = True
+# while running:
+while True:
+    try:
+        guess = int(input("Enter an integer:"))
+        if guess == number:
+            print("Congratulation, you guessed it.")
+            # running = False  # this causes the while loop to stop
+            break
+        elif guess < number:
+            print("It is greater than your input ,please guess again !")
+        else:
+            print("It is less than your input, please guess again !")
+    except ValueError:
+        print("pls input integer")
 else:
-  print("the while is over.")
-print("Done!")
+    print("the while is over.")
+print("Game Over !")
